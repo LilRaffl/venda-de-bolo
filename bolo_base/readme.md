445c6a8c2c5e48c2b2e30f396645c87a4606d2ba
LilRaffl <caninosbrancoscb10@gmail.com>
2022-06-03 13:50:39 -0300

    projeto 1

---
+++ bolo_base/readme.md
@@ -0,0 +1,6 @@
+## projeto de Compra de Bolo desenvolvido em JavaScript
+
+## Pagina Inicial
+
+
+<img src="./images/inicial.png" alt="img bolo'> 
---
+++ bolo_base/script.js
@@ -0,0 +1,89 @@
+const c = (el) => document.querySelector(el)
+const cs = (el) => document.querySelectorAll(el)
+let qt= 1
+
+
+
+
+cakeJson.map((item, index) =>{
+    
+    let cakeItem = document.querySelector('.models .cake-item ') .cloneNode(true)
+
+ cakeItem.setAttribute('data-key',index)
+
+
+    
+    //pegando nome do bolo
+    cakeItem.querySelector('.cake-item--name').innerHTML = item.name
+   
+  //descriçao
+  cakeItem.querySelector('.cake-item--desc').innerHTML = item.description
+
+  //imagen
+  cakeItem.querySelector('.cake-item--img img').src = item.img
+
+    cakeItem.querySelector('.cake-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+
+    c('.cake-area').append(cakeItem)
+
+    //janela
+
+    cakeItem.querySelector('a').addEventListener('click',(e) =>{
+    e.preventDefault()
+        
+    let key = e.target.closest('.cake-item').getAttribute('data-key')
+
+     //nome do bolo
+    
+      c('.cakeInfo h1 ').innerHTML = cakeJson[key].name
+     c('.cakeInfo--actualPrice').innerHTML =  `R$ ${cakeJson[key].price.toFixed(2)}`
+     c('.cakeBig img').src = cakeJson[key].img 
+
+
+     cs('.cakeInfo--size').forEach((size,sizeIndex)=>{
+     size.querySelector('span').innerHTML = cakeJson[key].sizes[sizeIndex]
+     })
+    
+     c('.cakeInfo--qt').innerHTML = qt
+     c('.cakeWindowArea').style.opacity = 0
+     c('.cakeWindowArea').style.display = 'flex'
+      
+        
+     setTimeout(( )  =>{
+     c('.cakeWindowArea').style.opacity = 1
+     }, 200)
+
+
+    })
+
+
+
+})
+
+function closeModal(){
+    c('.cakeWindowArea').style.opacity = 0
+
+    setTimeout(( )  =>{
+        c('.cakeWindowArea').style.display= 'none'
+        
+    }, 500)
+
+}
+
+cs('.cakeInfo--cancelButton, .cakeInfo--cancelMobileButton').forEach((item)=>{
+ item.addEventListener('click',closeModal)
+})
+
+c('.cakeInfo--qtmenos').addEventListener('click',()=>{
+
+    if(qt > 1){
+        qt--
+        c('.cakeInfo--qt').innerHTML = qt
+
+    }
+})
+
+c('.cakeInfo--qtmais').addEventListener('click',()=>{
+    qt++
+    c('.cakeInfo--qt').innerHTML = qt
+})
